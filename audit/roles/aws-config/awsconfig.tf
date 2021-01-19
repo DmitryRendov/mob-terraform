bb7e84154b385e6759f36c867c8e7065e109b39d
Dmitry Rendov <DmitryRendov@users.noreply.github.com>
2021-01-19 11:10:17 +0300

    15 - Enable Config for multi region (#16)

--- audit/roles/aws-config/awsconfig.tf
+++ audit/roles/aws-config/awsconfig.tf
@@ -17,17 +17,21 @@ module "aws_config" {
   }
 }
 
-#module "aws_config_east" {
-#  source          = "../../../modules/base/aws-config/v1"
-#  aws_account_id  = var.aws_account_id
-#  aws_account_map = var.aws_account_map
+module "aws_config_west" {
+  source          = "../../../modules/base/aws-config/v1"
+  aws_account_id  = var.aws_account_id
+  aws_account_map = var.aws_account_map
 
-#  role_name                 = var.account_name
-#  environment               = terraform.workspace
-#  account_name              = var.account_name
-#  cr_s3_bucket              = data.terraform_remote_state.audit.outputs.aws_config_bucket.id
+  role_name    = var.account_name
+  environment  = terraform.workspace
+  account_name = var.account_name
 
-#  providers = {
-#    aws = aws.west
-#  }
-#}
+  # Config Recorder settings
+  cr_is_enabled         = true
+  cr_delivery_frequency = "TwentyFour_Hours"
+  cr_s3_bucket          = data.terraform_remote_state.audit.outputs.aws_config_bucket.id
+
+  providers = {
+    aws = aws.west
+  }
+}
