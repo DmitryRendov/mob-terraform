e96d5efe7ea14078f81d4411a5a81f521ad6e0d3
DmitryRendov <drendov@gmail.com>
2021-02-09 10:44:08 +0300

    22 - Allow to disable/enable AWS Security Hub

--- audit/global/global/locals.tf
+++ audit/global/global/locals.tf
@@ -2,6 +2,8 @@ locals {
   role_name = "global"
   team      = "ops"
 
+  security_hub_enabled = false
+
   config_recorder_enabled            = false
   config_recorder_delivery_frequency = "TwentyFour_Hours"
 }
--- bastion/global/global/security_hub.tf
+++ bastion/global/global/security_hub.tf
@@ -1,26 +1,33 @@
-resource "aws_securityhub_account" "us_east_1" {}
+resource "aws_securityhub_account" "us_east_1" {
+  count = local.security_hub_enabled ? 1 : 0
+}
 
 resource "aws_securityhub_standards_subscription" "cis_us_east_1" {
+  count         = local.security_hub_enabled ? 1 : 0
   depends_on    = [aws_securityhub_account.us_east_1]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
 
 resource "aws_securityhub_standards_subscription" "aws_foundational_us_east_1" {
+  count         = local.security_hub_enabled ? 1 : 0
   depends_on    = [aws_securityhub_account.us_east_1]
   standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
 }
 
 resource "aws_securityhub_account" "us_west_2" {
+  count    = local.security_hub_enabled ? 1 : 0
   provider = aws.west
 }
 
 resource "aws_securityhub_standards_subscription" "cis_us_west_2" {
+  count         = local.security_hub_enabled ? 1 : 0
   depends_on    = [aws_securityhub_account.us_west_2]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
   provider      = aws.west
 }
 
 resource "aws_securityhub_standards_subscription" "aws_foundational_us_west_2" {
+  count         = local.security_hub_enabled ? 1 : 0
   depends_on    = [aws_securityhub_account.us_west_2]
   standards_arn = "arn:aws:securityhub:us-west-2::standards/aws-foundational-security-best-practices/v/1.0.0"
   provider      = aws.west
